0318b6955db34e656c75349f520a2ff5e239fb6b
amaneasterling <aeasterling26@scienceleadership.org>
2025-09-11 09:13:48 -0400

    Make changes to Player

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         Dealer dealer = new Dealer();
+        dealer.name = "Luke";
+
         Player player1 = new Player();
         Player player2 = new Player();
 
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,19 @@
+import java.util.Random;
 public class Player {
     int score;
     String name;
+    Random r = new Random();
+
+    int bet;
+
+        void hit(){
+            score+=r.nextInt(2,11);
+
+        }
+       void stay(){
+
+       }
+        void doubleBet(){
+
+        }
 }
