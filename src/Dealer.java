12928bc9cecf292371e45d787a628302fab145a4
AlexChancey <achancey27@scienceleadership.org>
2025-09-11 08:53:41 -0400

    added stuff

--- src/Dealer.java
+++ src/Dealer.java
@@ -11,7 +11,15 @@ public class Dealer {
 
     void deal(Player recipient){
         int card = r.nextInt(2,11);
-        System.out.println(recipient.name+"'s firstcard");
+        System.out.println(recipient.name+"'s first card is a " +card);
+        recipient.score+=card;
+        card = r.nextInt(2,11);
+        System.out.println(recipient.name+"'s second card is a " +card);
+        recipient.score+=card;
+    }
+
+    void askName(Player player){
+        System.out.println("what is player 1's name?");
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Dealer dealer = new Dealer();
         Player player1 = new Player();
+        Player player2 = new Player();
+
+
+        dealer.greeting();
         dealer.deal(player1);
         }
     }
